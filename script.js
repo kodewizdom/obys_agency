cce9e08882e763c93639414ac639f3f9abfb6e47
sajid <mdskhan4774@gmail.com>
2024-02-14 15:56:50 +0530

    created loader page

---
+++ script.js
@@ -0,0 +1,54 @@
+function loadingAnimation(){
+  let tl = gsap.timeline()
+tl.from(".line h1", {
+  y: 150,
+  stagger: 0.25,
+  duration: 0.6,
+  delay: 0.5,
+})
+
+tl.from("#line1-part1", {
+  opacity: 0,
+  onStart: function () {
+    let h5timer = document.querySelector('#line1-part1 h5')
+    let grow = 0;
+    setInterval(function () {
+      if (grow < 100) {
+        h5timer.innerHTML = grow++
+      } else {
+        h5timer.innerHTML = grow
+      }
+    }, 35)
+  },
+});
+
+tl.to(".line h2",{
+  animationName:'anime',
+  opacity:1,
+})
+
+tl.to("#loader", {
+  opacity: 0,
+  duration: 0.2,
+  delay: 4,
+})
+
+tl.from('#page1', {
+  delay: 0.2,
+  y: 1200,
+  opacity: 0,
+  duration: 0.5,
+  // ease:power9
+})
+
+tl.to("#loader",{
+  display:"none"
+})
+}
+
+loadingAnimation()
+
+
+
+
+
